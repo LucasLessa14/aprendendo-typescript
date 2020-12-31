0ab1a034c258d3ccf1473b9484214d857d90a75c
Lucas Lessa <lucaslessa14@hotmail.com>
2020-12-31 15:33:09 -0300

    Exercicio (concessionaria/carro/pessoa)

---
+++ HelloWorld/dist/app.js
@@ -0,0 +1,60 @@
+"use strict";
+var Carro = /** @class */ (function () {
+    function Carro(modelo, numeroDePortas) {
+        this.velocidade = 0;
+        this.modelo = modelo;
+        this.numeroDePortas = numeroDePortas;
+    }
+    Carro.prototype.acelerar = function () {
+        this.velocidade = this.velocidade + 10;
+    };
+    Carro.prototype.parar = function () {
+        this.velocidade = 0;
+    };
+    Carro.prototype.velocidadeAtual = function () {
+        return this.velocidade;
+    };
+    return Carro;
+}());
+var Concessionaria = /** @class */ (function () {
+    function Concessionaria(endereco) {
+        this.endereco = endereco;
+    }
+    Concessionaria.prototype.fornecerEndereco = function () {
+        return this.endereco;
+    };
+    Concessionaria.prototype.mostrarListaDeCarros = function () {
+        return this.listaDeCarros;
+    };
+    return Concessionaria;
+}());
+;
+var Pessoa = /** @class */ (function () {
+    function Pessoa(nome, carroPreferido) {
+        this.nome = nome;
+        this.carroPreferido = carroPreferido;
+    }
+    Pessoa.prototype.dizerNome = function () {
+        return this.nome;
+    };
+    Pessoa.prototype.dizerCarroPreferido = function () {
+        return this.carroPreferido;
+    };
+    Pessoa.prototype.comprarCarro = function (carro) {
+        this.carro = carro;
+    };
+    Pessoa.prototype.dizerCarroQueTem = function () {
+        return this.carro;
+    };
+    return Pessoa;
+}());
+var concessionaria = new Concessionaria("Av. Gomes de Matos");
+console.log(concessionaria);
+var carro = new Carro('Palio', 2);
+console.log(carro);
+var pessoa = new Pessoa("José", "Ferrari");
+console.log(pessoa);
+var ola = function (texto) {
+    console.log(texto);
+};
+ola("Hello World");
---
+++ README.md
@@ -0,0 +1,38 @@
+# TypeScript
+
+O que é TypeScript?
+---
+TypeScript é um superconjunto de JavaScript desenvolvido pela Microsoft que adiciona tipagem e alguns outros recursos a linguagem. (By Google)
+
+## Inferência de Tipos
+
+---
+
+No javascript, o tipo da variável se adapta com base no tipo do dado que é atribuído a ela. No entanto, essa característica do JavaScript pode resultar em problemas em ambiente de produção. A tipagem de variáveis vem se mostrando uma boa prática e a proposta do TypeScript é trazer essa tipagem de variáveis para o JavaScript.
+
+## Verificação Estática
+
+---
+
+Ao atribuir tipos as variáveis, o typescript faz advertências em tempo real. Ele não permite que você atribua um valor com tipo diferente a uma variável.
+
+## Suporte a Classes e Interfaces
+
+---
+
+Orientação a Objetos é um paradigma da programação que tratar trechos de códigos como objetos do mundo real.
+
+## Código
+
+---
+
+``` javascript
+npm install -g typescript
+
+tsc app // Informar a extensão é opcional
+
+tsc -init // Inicia um projeto typescript com tsconfig.json
+```
+
+## Autor
+- Lucas Lessa
